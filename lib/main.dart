a50fed5d2fd67a982a47757b88b57af85cb1a638
Trey Hope <trey.a.hope@gmail.com>
2021-12-20 01:09:25 -0500

    * Added new_version package.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:package_info/package_info.dart';
 import 'blocs/complaint_form/complaint_form_bloc.dart';
 import 'constants.dart';
+import 'package:new_version/new_version.dart';
+
+final _newVersion = NewVersion();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +14,6 @@ void main() async {
   setUpLocator();
 
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
-
   version = packageInfo.version;
   buildNumber = packageInfo.buildNumber;
 
@@ -21,6 +23,8 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    _newVersion.showAlertIfNecessary(context: context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
